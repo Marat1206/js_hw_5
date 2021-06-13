43703010e230103f57ebbee5c6c90226c9141c39
Marat1206 <marikzhekov@gmail.com>
2021-06-13 22:57:00 +0300

    init

---
+++ cycles/for.js
@@ -0,0 +1,26 @@
+// let a = 1;
+// for (; a < 101; a++) {
+//     alert(a)
+// }
+
+// let a = 2;
+// for (; a < 101; a += 2){
+//     alert(a);
+// }
+//
+
+//
+// const arr = [1, 2, 3, 4, 5];
+// let sum = 0;
+// for (let a = 0; a <= arr.length- 1; a++) {
+//     sum += arr[a];
+// }
+// alert(sum);
+
+// const arr = [1, 2, 3, 4, 5];
+//  let sum = 0;
+//  for (let i = 0; i <= arr.length -1; i++) {
+//
+//      sum += arr[i] ** 2;
+//  }
+//  alert(sum);
